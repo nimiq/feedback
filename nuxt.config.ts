fe99f10359ee754e27e80f3727572f5382cd5659
onmax <maximogarciamtnez@gmail.com>
2025-05-14 15:10:41 +0200

    chore: added nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,14 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image', '@vueuse/nuxt', '@unocss/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@nuxthub/core',
+  ],
   future: { compatibilityVersion: 4 },
 
   eslint: {
