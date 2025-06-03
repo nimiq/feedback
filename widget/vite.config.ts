352177e232e4a6555112619b4eb8def61cec69b7
onmax <maximogarciamtnez@gmail.com>
2025-06-03 12:38:47 +0300

    chore: removed css wrap

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -1,5 +1,3 @@
-import type { Plugin } from 'vite'
-import { readFileSync, writeFileSync } from 'node:fs'
 import { join, resolve } from 'node:path'
 import process from 'node:process'
 import { fileURLToPath } from 'node:url'
@@ -30,7 +28,6 @@ export default defineConfig({
       'preventAssignment': true,
     }),
     Uno({ configFile: '../uno.config.ts' }),
-    cssWrapperPlugin(),
   ],
   root: __dirname, // Directs Vite to use the 'widget' folder as the project root
   optimizeDeps: {
@@ -53,19 +50,3 @@ export default defineConfig({
     },
   },
 })
-
-// Custom plugin to wrap CSS with selector. This is a trick to increase the selector specificity
-// and avoid conflicts with other styles on the page.
-function cssWrapperPlugin() {
-  return {
-    name: 'css-wrapper',
-    writeBundle() {
-      // Read the widget.css and wrap it
-      const filePath = join(outputFolder, 'widget.css')
-      console.log(`Wrapping CSS file: ${filePath}`)
-      const content = readFileSync(filePath)
-      const wrappedContent = `#feedback-widget{${content}}`
-      writeFileSync(filePath, wrappedContent)
-    },
-  } satisfies Plugin
-}
