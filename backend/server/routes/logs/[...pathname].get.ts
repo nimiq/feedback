19d3c62b94a4d9bfd67cec78a9185599c3820653
onmax <maximogarciamtnez@gmail.com>
2025-06-05 13:18:10 +0300

    fix: ensure pathname defaults to an empty string in route handlers

--- backend/server/routes/logs/[...pathname].get.ts
+++ backend/server/routes/logs/[...pathname].get.ts
@@ -1,5 +1,5 @@
 export default eventHandler(async (event) => {
-  const { pathname } = getRouterParams(event)
+  const { pathname = '' } = getRouterParams(event)
 
   // TODO improve security
   setHeader(event, 'Content-Security-Policy', 'default-src \'none\';')
