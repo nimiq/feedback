a26ca687551eb2dac3d7c4ded5c54774af1021a9
onmax <maximogarciamtnez@gmail.com>
2025-06-05 13:14:13 +0300

    chore: add logs pathname

---
+++ backend/server/routes/logs/[...pathname].get.ts
@@ -0,0 +1,8 @@
+export default eventHandler(async (event) => {
+  const { pathname } = getRouterParams(event)
+
+  // TODO improve security
+  setHeader(event, 'Content-Security-Policy', 'default-src \'none\';')
+
+  return hubBlob().serve(event, pathname)
+})
