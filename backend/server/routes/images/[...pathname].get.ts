6a71a07f9ff6177bd200f8f6b5a02c11054d3187
onmax <maximogarciamtnez@gmail.com>
2025-06-05 12:54:06 +0300

    chore: updated routes

--- backend/server/routes/images/[...pathname].get.ts
+++ backend/server/routes/images/[...pathname].get.ts
@@ -1,19 +1,8 @@
 export default eventHandler(async (event) => {
   const { pathname } = getRouterParams(event)
 
-  // pathname is an array for catch-all routes
-  const pathString = Array.isArray(pathname) ? pathname.join('/') : pathname
-
-  if (!pathString) {
-    return createError({ statusCode: 404, statusMessage: 'Not found' })
-  }
-
-  const [firstPart] = pathString.split('/')
-  if (!firstPart || (firstPart !== 'images' && firstPart !== 'logs'))
-    return createError({ statusCode: 404, statusMessage: 'Not found' })
-
   // TODO improve security
   setHeader(event, 'Content-Security-Policy', 'default-src \'none\';')
 
-  return hubBlob().serve(event, pathString)
+  return hubBlob().serve(event, pathname)
 })
